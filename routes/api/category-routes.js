1be70d370486d6975ff14db3ca6090a59b40393a
Dani <danisweetwood@gmail.com>
2024-02-12 19:12:57 -0600

    added routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,7 +44,7 @@ router.put('/:id', async (req, res) => {
       },
     });
     if (!categoryData[0]) {
-      res.status(404).json({ message: 'No user with this id!' });
+      res.status(404).json({ message: 'No category with this id!' });
       return;
     }
     res.status(200).json(categoryData);
@@ -62,7 +62,7 @@ router.delete('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No location found with this id!' });
+      res.status(404).json({ message: 'No category with this id!' });
       return;
     }
 
